e2a1e10ac1ebcbd0779c46907ca1e3d738a5543d
Vaclav Mecerod <vmecerod@alps.cz>
2018-11-09 15:03:54 +0100

    add grid

---
+++ temp-app/src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app" :class="{'collapsed' : collapsed}">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <test msg= "Hello msg"></test> -->
+    <!-- <users></users> -->
+    <sidebar-menu :menu="menu" :collapsed="collapsed" @collapse="onCollapse" />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Test from './components/test'
+import Users from './components/users'
+
+export default {
+  name: 'App',
+  components: {
+    Test,
+    Users
+  },
+  data() {
+    return {
+      menu: [
+        {
+          href: '/',
+          title: 'Dashboard',
+          icon: 'fa fa-user'
+        },
+        {
+          href: '/charts',
+          title: 'Charts',
+          icon: 'fa fa-chart-area'
+        },
+        {
+          href: '/tables',
+          title: 'Tables',
+          icon: 'fa fa-table'
+        },
+        {
+          href: '/users',
+          title: 'Users',
+          icon: 'fa fa-table'
+        },
+        {
+          href: '/test',
+          title: 'Only Test component',
+          icon: 'fa fa-table'
+        },
+      ],
+      collapsed: false
+    }
+  },
+  methods: {
+    onCollapse(val) {
+      console.log(`collapsed ${val}`)
+      this.collapsed = val
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600');
+
+body,html {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  padding-left: 350px;
+}
+#app.collapsed {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  padding-left: 50px;
+}
+</style>
---
+++ temp-app/src/main.js
@@ -0,0 +1,59 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from  'vue-resource'
+import VueSidebarMenu from 'vue-sidebar-menu'
+import VueGridLayout from 'vue-grid-layout'
+import App from './App'
+import Test from './components/test'
+import Users from './components/users'
+import Dashboard from './components/dashboard'
+// Vue.config.productionTip = false
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+Vue.use(VueSidebarMenu)
+Vue.use(VueGridLayout)
+
+// const Dashboard = { template: '<div>Dashboard Page</div>' }
+const Charts = { template: '<div>Charts Page</div>' }
+const Tables = { template: '<div>Tables Page</div>' }
+
+const router = new VueRouter({
+	routes: [
+    {
+      path: '/',
+      name: 'Dashboard',
+      component: Dashboard,
+    },
+    {
+      path: '/charts',
+      name: 'Charts',
+      component: Charts,
+    },
+    {
+      path: '/tables',
+      name: 'Tables',
+      component: Tables,
+    },
+    {
+      path: '/users',
+      name: 'Users',
+      component: Users,
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test,
+    },
+  ]
+})
+
+/* eslint-disable no-new */
+new Vue({
+	el: '#app',
+	router,
+	render: h => h(App),
+	
+});
